3721cf064fae93bfc909e5133842475f7d60bbab
James "Jimmy" Liao <62347264+kingofchardonnaynay@users.noreply.github.com>
2024-09-29 07:58:47 -0400

    Update model_utils.py

--- src/model_utils.py
+++ src/model_utils.py
@@ -1,14 +1,28 @@
 import json
+import gcsfs
 from tensorflow.keras.models import load_model
 
 # --- LOAD MODEL ---
 def load_symbol_model():
-    # Load the pretrained Keras model for symbol recognition
-    model = load_model(r"C:\Users\Jimmy\Desktop\final-project\PokemonTCG\models\model04.keras")
+    # Set up Google Cloud Storage FileSystem
+    fs = gcsfs.GCSFileSystem()
+
+    # Path to the model in GCS
+    gcs_model_path = "gs://jl-final-project/model04.keras"
+
+    # Open the model file from GCS
+    with fs.open(gcs_model_path, 'rb') as model_file:
+        model = load_model(model_file)
+
     return model
 
 # Load class names from JSON
 def load_class_names():
-    with open(r'C:\Users\Jimmy\Desktop\final-project\class_indices.json', 'r') as f:
-        class_indices = json.load(f)
-    return {v: k for k, v in class_indices.items()}
+    url = https://github.com/kingofchardonnaynay/james-liao-final-project/blob/main/data/models/class_indices.json
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        class_indices = response.json()
+        return {v: k for k, v in class_indices.items()}
+    else:
+        raise Exception(f"Error fetching class indices from GitHub: {response.status_code}")

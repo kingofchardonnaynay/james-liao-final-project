689d91ed3b6042a7a619a132a533eee4bc5a4f8a
James "Jimmy" Liao <62347264+kingofchardonnaynay@users.noreply.github.com>
2024-09-29 07:26:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-from app import main  # Import the main function or logic from app.py
+from app import main
 
 if __name__ == "__main__":
-    main()  # Adjust this to match how you run your app in app.py
+    main()
